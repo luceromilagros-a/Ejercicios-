e9cf405841fc5eeff9690efdf2a378c1b61f20c6
Milagros Lucero <luceromilagros.a@gmail.com>
2026-04-29 16:15:40 -0300

    feat: ejercicio validación de mayotía de edad

--- Ejercicios/ejerciciosU3.py
+++ Ejercicios/ejerciciosU3.py
@@ -95,4 +95,12 @@ if n1 == n2:
 else :
     print ("Los número son diferentes")
 
-          
+# 7. Pregunta al usuario que ingrese su edad. 
+# Si la edad es mayor o igual a 18, imprime "Eres mayor de edad". 
+# De lo contrario, imprime "Eres menor de edad".
+
+edad = int (input ("Ingresá tu edad: "))
+if edad >= 18 :
+    print ("Eres mayor de edad")
+else :
+    print ("Eres menor de edad") 
